f3ea42bb757137647d0a054cb74368f703a84b97
Wood-Jack <thewoodrowz@gmail.com>
2021-09-28 10:35:47 -0400

    changes

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -6,7 +6,7 @@ import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from './rootReducer';
 
 // eslint-disable-next-line no-undef - because redux said is important to do
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 
 const rootStore = createStore(
   rootReducer, composeEnhancers(applyMiddleware(promiseMiddleware)));
